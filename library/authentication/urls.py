8099e24a757e566bd7b4d260ee1c8c99bc129af8
Yura Kaptar <mrnarrow17@gmail.com>
2026-08-26 13:00:35 +0300

    Copied over files from previous sprint

---
+++ library/authentication/urls.py
@@ -0,0 +1,12 @@
+from django.urls import path
+
+from . import views
+
+app_name = "auth"
+
+urlpatterns = [
+    path('', views.login_view, name='login'),
+    path('register/', views.register_view, name='register'),
+    path('logout/', views.logout_view, name='logout'),
+    path('edit/', views.edit_view, name='edit')
+]
---
+++ library/author/views.py
@@ -0,0 +1,54 @@
+from django.contrib import messages
+from django.contrib.auth import get_user_model
+from django.contrib.auth.decorators import user_passes_test
+from django.core.exceptions import PermissionDenied
+from django.http import HttpRequest, HttpResponse
+from django.shortcuts import redirect, render
+
+from .forms import AuthorForm
+from .models import Author
+
+User = get_user_model()
+
+
+def is_admin(user) -> bool:
+    if user.is_staff:
+        return True
+    raise PermissionDenied
+
+
+@user_passes_test(is_admin)
+def author_list(request: HttpRequest) -> HttpResponse:
+    authors = Author.objects.all()
+    form = AuthorForm()
+    return render(
+        request, "author/author_list.html", {"form": form, "authors": authors}
+    )
+
+
+@user_passes_test(is_admin)
+def author_create(request: HttpRequest) -> HttpResponse:
+    if request.method == "POST":
+        form = AuthorForm(request.POST)
+        if form.is_valid():
+            form.save()
+            messages.success(request, "Author added successfully!")
+            return redirect("author:list")
+        messages.error(request, "Invalid creation data")
+
+    return author_list(request)
+
+
+@user_passes_test(is_admin)
+def author_delete(request: HttpRequest, id: int) -> HttpResponse:
+    author = Author.objects.get(pk=id)
+    if author.books.count() != 0:
+        messages.error(request, "Delete the author's books first")
+    else:
+        author.delete()
+    return author_list(request)
+
+
+# @user_passes_test(is_admin)
+# def author_books(request: HttpRequest, id: int) -> HttpResponse:
+#     return render(request, "books/book_list", {"search_author": id})
