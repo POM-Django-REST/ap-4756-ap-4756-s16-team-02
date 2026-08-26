6b29f60e1077f61cdd53dad04eaa2ca70a68c578
Yura Kaptar <mrnarrow17@gmail.com>
2026-08-26 14:25:40 +0300

    Added api endpoints to book app

--- library/book/api_urls.py
+++ library/book/api_urls.py
@@ -5,5 +5,5 @@ from . import api
 app_name = "book"
 
 urlpatterns = [
-    path("<int:id>/", api.book_detail, name="book_detail"),
+    path("<int:id>/", api.BookDetailsView.as_view(), name="book_detail"),
 ]
