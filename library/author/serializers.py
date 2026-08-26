08fbaa73d8c73587670c2f6255cd72ad7523e9a9
Maksym Chumachenko <makchumachenko@gmail.com>
2026-08-26 22:11:48 +0300

    Catching model/db integrity errors

--- library/author/serializers.py
+++ library/author/serializers.py
@@ -1,4 +1,5 @@
 from rest_framework import serializers
+from django.db import IntegrityError
 
 from .models import Author
 
@@ -7,3 +8,19 @@ class AuthorSerializer(serializers.ModelSerializer):
     class Meta:
         model = Author
         fields = ['id', 'name', 'surname', 'patronymic']
+
+    def create(self, validated_data):
+        try:
+            return super().create(validated_data)
+        except IntegrityError as e:
+            raise serializers.ValidationError(
+                {"detail": str(e)}
+            )
+
+    def update(self, instance, validated_data):
+        try:
+            return super().update(instance, validated_data)
+        except IntegrityError as e:
+            raise serializers.ValidationError(
+                {"detail": str(e)}
+            )
--- library/user/serializers.py
+++ library/user/serializers.py
@@ -1,5 +1,6 @@
 from django.contrib.auth import get_user_model
 from rest_framework import serializers
+from django.db import IntegrityError
 
 User = get_user_model()
 
@@ -8,3 +9,19 @@ class UserSerializer(serializers.ModelSerializer):
         model = User
         fields = ['id', 'first_name', 'middle_name', 'last_name', 'email',
                 'created_at', 'updated_at', 'role', 'is_active']
+
+    def create(self, validated_data):
+        try:
+            return super().create(validated_data)
+        except IntegrityError as e:
+            raise serializers.ValidationError(
+                {"detail": str(e)}
+            )
+
+    def update(self, instance, validated_data):
+        try:
+            return super().update(instance, validated_data)
+        except IntegrityError as e:
+            raise serializers.ValidationError(
+                {"detail": str(e)}
+            )
